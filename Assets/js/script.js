f7f7d2de086f76fa84b2eaf06af618fcd67971cb
nbross <nbross2001@gmail.com>
2022-03-11 22:56:13 -0500

    updated character variables so they are arrays

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // arrays of possible characters
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numbers = "0123456789";
-var special = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 function generatePassword() {
   var password = "";
