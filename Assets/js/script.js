6da80ea56b1e532d2cf17151cb0798ef8ab12d90
nbross <nbross2001@gmail.com>
2022-03-11 20:47:14 -0500

    changed realtive paths in html for css and js, created initial function for generate password and wrote psuedocode

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey! you pressed the button!");
+
+  // 1. Prompt the user for the password criteria
+  //    a. Password length 8 < 128
+  //    b. Lowercase, Uppercase, Numbers, Special
+  // 2. Validate the input.
+  // 3. Generate password based on criteria
+  // 4. Display password to the page.
+
+  return "Generated Password will go here!";
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
